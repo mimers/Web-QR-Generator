a35d4358c336bd706ab75abefc2cdd3b4448924a
李少杰 <super@joker.li>
2018-12-13 19:26:30 +0800

    Support share QR codes

--- script.js
+++ script.js
@@ -1,102 +1,85 @@
-// ==UserScript==
-// @name         Google在线二维码服务
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://chart.googleapis.com/chart
-// @grant        none
-// @run-at       document-end
-// ==/UserScript==
+'use strict';
+const prefix = 'https://chart.googleapis.com/chart?cht=qr&chs=400x400&chl=';
 
-(function() {
-    'use strict';
-    const prefix = 'https://chart.googleapis.com/chart?cht=qr&chs=400x400&chl=';
-    document.body.innerHTML = '';
-    let localQr = JSON.parse(localStorage.getItem('_local_qr_codes_') || '["hello, world"]');
-    document.title = '简洁强大的二维码生成器';
+function updateLocalData() {
+  let ds = [];
+  document.querySelectorAll('.qr-img').forEach(i => ds.push(i.chl));
+  localStorage.setItem('_local_qr_codes_', JSON.stringify(ds.map(i => i)));
+}
 
-    function updateLocalData() {
-        let ds = [];
-        document.querySelectorAll('.qr-img').forEach(i => ds.push(i.chl))
-        localStorage.setItem('_local_qr_codes_', JSON.stringify(ds.map(i => i)));
+function bindImage(img, chl) {
+  img.src = prefix + encodeURIComponent(chl);
+  img.chl = chl;
+}
+
+function createQR(chl) {
+  let container = document.createElement('div');
+  container.className = 'qr-container';
+  let input = document.createElement('textarea');
+  input.className = 'qr-input';
+  let qrImg = document.createElement('img');
+  qrImg.className = 'qr-img';
+  if (chl) {
+    bindImage(qrImg, chl);
+    input.value = chl;
+  }
+  qrImg.addEventListener('click', function (e) {
+    if (e.altKey) {
+      container.parentNode.removeChild(container);
+      updateLocalData();
     }
+  });
 
-    function bindImage(img, chl) {
-        img.src = prefix + encodeURIComponent(chl);
-        img.chl = chl;
-    }
+  container.appendChild(input);
+  container.appendChild(qrImg);
+  input.addEventListener('change', function (e) {
+    let t = e.target.value.trim();
+    bindImage(qrImg, t);
+    updateLocalData();
+  });
+  return container;
+}
 
-    function createQR(chl) {
-        let container = document.createElement('div');
-        container.className = 'qr-container';
-        let input = document.createElement('textarea');
-        input.className = 'qr-input';
-        let qrImg = document.createElement('img');
-        qrImg.className = 'qr-img';
-        if (chl) {
-            bindImage(qrImg, chl);
-            input.value = chl;
-        }
-        qrImg.addEventListener('click', function(e) {
-            if (e.altKey) {
-                container.parentNode.removeChild(container);
-                updateLocalData();
-            }
-        });
+function parseQrFromQuery() {
+  const query = new URLSearchParams(location.search);
+  let qrs = query.get('qrs');
+  try {
+    return JSON.parse(qrs);
+  } catch (e) {
+    console.error(e);
+  }
+  return null;
+}
 
-        container.appendChild(input);
-        container.appendChild(qrImg);
-        input.addEventListener('change', function(e) {
-            let t = e.target.value.trim();
-            bindImage(qrImg, t);
-            updateLocalData();
-        })
-        return container;
-    }
+function clearSearch() {
+  location.href = location.href.substr(0, location.href.length - location.search.length);
+}
 
-    let add = document.createElement('button');
-    add.textContent = '添加二维码生成器';
-    add.addEventListener('click', function() { document.body.insertBefore(createQR('voilà'), add) });
-    document.body.appendChild(add);
-    localQr.forEach(chl => document.body.insertBefore(createQR(chl), add));
+function init() {
+  let add = document.createElement('button');
+  add.className = 'qr-add';
+  add.textContent = '添加二维码生成器';
+  add.addEventListener('click', () => {
+    document.body.insertBefore(createQR('voilà'), add)
+  });
+  let share = document.querySelector('button.qr-share');
+  share.addEventListener('click', (e) => {
+    const query = new URLSearchParams();
+    query.append('qrs', localStorage.getItem('_local_qr_codes_'));
+    navigator.clipboard.writeText(location.href + '?' + query.toString());
+    let origin = e.target.textContent;
+    e.target.textContent = '链接已复制~';
+    setTimeout(() => e.target.textContent = origin, 3000);
+  });
+  document.body.appendChild(add);
+  let localQr = JSON.parse(localStorage.getItem('_local_qr_codes_') || '["hello, world"]');
+  let queryQr = parseQrFromQuery();
+  localQr.forEach(chl => document.body.insertBefore(createQR(chl), add));
+  if (queryQr) {
+    queryQr.filter(q => !localQr.find(i => i === q)).forEach(chl => document.body.insertBefore(createQR(chl), add));
+    updateLocalData();
+    setTimeout(clearSearch, 300);
+  }
+}
 
-    document.head.querySelector('style').innerHTML = `
-body {
-    display: flex;
-    background: white;
-    margin: 0;
-    padding: 8px;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: space-evenly;
-}
-.qr-container {
-    box-shadow: 0px 2px 3px 0px lightgray, 0px 0px 2px 0px lightgray;
-    border-radius: 0px 0px 4px 4px;
-    margin: 12px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-.qr-input {
-    width: 400px;
-    height: 60px;
-    word-break: break-all;
-}
-.qr-img {
-    height: 400px;
-    width: 400px;
-    transition: filter ease .5s;
-    border-radius: 0px 0px 4px 4px;
-}
-.qr-input:focus + .qr-img {
-    filter:  blur(13px);
-}
-button {
-    height: 100px;
-    width: 100px;
-    font-size: 20px;
-}
-`;
-})();
+init();
